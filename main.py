7114708ea2c5e1262d7b5e29655635a09ac3d063
Olivia Stensota <olisten@student.chalmers.se>
2022-09-30 15:11:34 +0200

    get all opening characters

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 
 
+from tempfile import TemporaryDirectory
+
+
 f = open("input.txt", "r")
 
-print(f.readlines())
+#print(f.readlines())
+string = f.readlines()
 #print(f.readline())
+
+open = ["(", "[", "{", "<"]
+close = [")", "]", "}", ">"]
+
+temp_list = []
+
+print(string[1])
+
+for element in string[1]:
+    print(element)
+    if element in open:
+        temp_list.append(element)
+
+print(temp_list)
